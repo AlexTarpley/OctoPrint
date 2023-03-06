73b6d01f3a49e9a595e06258a60161c8a1e5ee09
Alex Tarpley <alex.tarpley@du.edu>
2023-03-06 12:02:21 -0700

    Add octoprint printerinfo plugin
    Setup for new plugin

---
+++ src/octoprint/plugins/OctoPrint-Printerinfo/octoprint_printerinfo/__init__.py
@@ -0,0 +1,65 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+### (Don't forget to remove me)
+# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
+# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
+# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
+# as necessary.
+#
+# Take a look at the documentation on what other plugin mixins are available.
+
+import octoprint.plugin
+import requests
+
+class PrinterinfoPlugin(octoprint.plugin.StartupPlugin):
+
+    def on_after_startup(self):
+        profile = octoprint.printer.profile()
+        printer_name = profile.get("name")
+        self._logger.info(f"Printer name: {printer_name}")
+        return printer_name
+    
+
+    ##~~ Softwareupdate hook
+    def get_update_information(self):
+        # Define the configuration for your plugin to use with the Software Update
+        # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
+        # for details.
+        return {
+            "printerinfo": {
+                "displayName": "Printerinfo Plugin",
+                "displayVersion": self._plugin_version,
+
+                # version check: github repository
+                "type": "github_release",
+                "user": "AlexTarpley",
+                "repo": "OctoPrint-Printerinfo",
+                "current": self._plugin_version,
+
+                # update method: pip
+                "pip": "https://github.com/AlexTarpley/OctoPrint-Printerinfo/archive/{target_version}.zip",
+            }
+        }
+
+
+# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
+# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
+# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+
+
+
+# Set the Python version your plugin is compatible with below. Recommended is Python 3 only for all new plugins.
+# OctoPrint 1.4.0 - 1.7.x run under both Python 3 and the end-of-life Python 2.
+# OctoPrint 1.8.0 onwards only supports Python 3.
+__plugin_pythoncompat__ = ">=3,<4"  # Only Python 3
+
+def __plugin_load__():
+    global __plugin_implementation__
+    __plugin_implementation__ = PrinterinfoPlugin()
+
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+        "octoprint.plugin.on_after_startup": __plugin_implementation__.on_after_startup
+    }
